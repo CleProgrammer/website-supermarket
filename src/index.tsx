f5907494cb62fc98e3d4f74dd61f62c37a63e335
CleProgrammer <clebsonmengao@hotmail.com>
2023-09-15 14:13:41 -0300

    changes

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import  ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route, HashRouter as Router } from "react-router-dom";
 
 import Home from "./pages/home/home";
 import Searchpage from "./pages/searchpage/searchpage";
@@ -10,12 +10,12 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
-        <HashRouter>
+        <Router>
             <Routes>
                 <Route path="/" element={<Home/>}/>
                 <Route path="/searchpage" element={<Searchpage/>}/>
                 <Route path="/productpage" element={<Productpage/>}/>
             </Routes>
-        </HashRouter>
+        </Router>
     </React.StrictMode>
 )
